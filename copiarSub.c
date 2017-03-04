2297b5564d31918d4220719a57fc018df756a5c8
scocarojas <scocarojas@javerianacali.edu.co>
2017-03-04 17:45:47 -0500

    Update copiarSub.c

--- copiarSub.c
+++ copiarSub.c
@@ -6,7 +6,6 @@ char *copiarSub(char *origen,int n, int m){
 
 	int i=0;
 	char *destino;
-
 	destino=(char *)malloc(m-n+1);
 
 	for(i=n;i<=m;i++){
@@ -14,15 +13,10 @@ char *copiarSub(char *origen,int n, int m){
 	}
 	printf("%s\n",destino);
 }
-
 void main(int argc, char *argv[]){
-	char *cad1=argv[1];
-	
-	int n=atoi(argv[2]);
 
+	char *cad1=argv[1];
+	int n=atoi(argv[2]);
 	int m=atoi(argv[3]);
 	copiarSub(cad1,n,m);
 }
-	
-	
-	
