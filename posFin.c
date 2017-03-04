2d57b3619beda713c7425628bc2f7fb2d159ba56
scocarojas <scocarojas@javerianacali.edu.co>
2017-03-04 17:32:34 -0500

    Update posFin.c

--- posFin.c
+++ posFin.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <string.h>
 
-
-
 int posFin(char *origen, char *car){
-
-	
-	
-		
+			
 	int con=0;
 	int n=strlen(origen);
 	
@@ -20,13 +13,9 @@ int posFin(char *origen, char *car){
 	}
 	return con-1;
 }
-
-
 int main(int argc, char *argv[]){
 
-
 	char *cad1=argv[1];
-	
 	char *car=argv[2];
 	int n=posFin(cad1,car);
 	printf("%d",n);
