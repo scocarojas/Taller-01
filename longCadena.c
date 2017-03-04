b4c22ec2a59c2adfcac84996d6533f3fdf76a89a
scocarojas <scocarojas@javerianacali.edu.co>
2017-03-04 17:49:58 -0500

    Update longCadena.c

--- longCadena.c
+++ longCadena.c
@@ -1,21 +1,14 @@
 #include<stdio.h>
-
 #include<stdlib.h>
 
 int longCadena(char *cad){
 	
 	int n=0;
 	
-
-	while(cad[n]!='\0')
-		
-		n++;
-
-	
+	while(cad[n]!='\0')		
+		n++;	
 		return n;
-
 }
-
 int main(int argc, char *argv[]){
 	
 	char *cad1=argv[1];
